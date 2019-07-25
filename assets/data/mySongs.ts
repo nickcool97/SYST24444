59d4722a651a87e0fceb4acd8269b11c7994c9dc
Nicholas Dias <nickdias@hotmail.ca>
2019-07-24 22:02:15 -0400

    Add files via upload

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -6,7 +6,7 @@ import {songInfo} from '../data/songInfo';
       songArtist: 'Chris Brown',
       songGenre: 'Pop',
       songYear: 2019,
-      songArtwork: '../assets/images/No Guidance.png'
+      songArtwork: '../assets/images/no_guidance.png'
     },
 
     {
@@ -14,7 +14,7 @@ import {songInfo} from '../data/songInfo';
       songArtist: 'Drake',
       songGenre: 'Pop',
       songYear: 2018,
-      songArtwork: '../assets/images/Im Upset.png'
+      songArtwork: '../assets/images/im_upset.png'
     },
 
    {
@@ -22,7 +22,7 @@ import {songInfo} from '../data/songInfo';
       songArtist: 'Kevin Gates',
       songGenre: 'Pop',
       songYear: 2016,
-      songArtwork: '../assets/images/2 Phones.png'
+      songArtwork: '../assets/images/2_phones.png'
     },
 
      {
@@ -30,7 +30,7 @@ import {songInfo} from '../data/songInfo';
       songArtist: 'The Weeknd',
       songGenre: 'Pop',
       songYear: 2012,
-      songArtwork: '../assets/images/Wicked Games.jpg'
+      songArtwork: '../assets/images/wicked_games.jpg'
 
     }
 
